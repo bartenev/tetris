116d19bd818851e6d80b612fbbb5a75e0697e914
bartenev <bartenev.ilya@gmail.com>
2019-05-22 23:21:33 +0300

    Add files via upload

--- Assets/Scripts/Matrix Scripts/MatrixGrid.cs
+++ Assets/Scripts/Matrix Scripts/MatrixGrid.cs
@@ -2,17 +2,76 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class NewBehaviourScript : MonoBehaviour
+public class MatrixGrid : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public static int row = 10;
+    public static int column = 20;
+
+    public static Transform[,] grid = new Transform[row, column];
+
+    public static Vector2 RoundVector(Vector2 v)
     {
-        
+        return new Vector2(Mathf.Round(v.x), Mathf.Round(v.y));
     }
 
-    // Update is called once per frame
-    void Update()
+    public static bool IsInsideBorder(Vector2 pos)
     {
-        
+        return ((int)pos.x >= 0 && (int)pos.x < row && (int)pos.y >= 0);
+    }
+
+    public static void DeleteRow(int y)
+    {
+        for (int x = 0; x < row; ++x)
+        {
+            GameObject.Destroy(grid[x, y].gameObject);
+            grid[x, y] = null;
+        }
+    }
+
+    public static void DecreaseRow(int y)
+    {
+        for (int x = 0; x < row; ++x)
+        {
+            if (grid[x, y] != null)
+            {
+                grid[x, y - 1] = grid[x, y];
+                grid[x, y] = null;
+
+                grid[x, y - 1].position += new Vector3(0, -1, 0);
+            }
+        }
+    }
+
+    public static void DecreaseRowAbove(int y)
+    {
+        for (int i = y; i < column; ++y)
+        {
+            DecreaseRow(i);
+        }
+    }
+
+    public static bool IsFullRow(int y)
+    {
+        for (int x = 0; x < row; ++x)
+        {
+            if (grid[x, y] == null)
+            {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    public static void DeleteWholeRows()
+    {
+        for(int y = 0; y < column; ++y)
+        {
+            if(IsFullRow(y))
+            {
+                DeleteRow(y);
+                DecreaseRowAbove(y + 1);
+                --y;
+            }
+        }
     }
 }
